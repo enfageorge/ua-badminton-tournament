32c2d91535bc9256c75d822bd4631eea14bd626f
Junfeng Xu <junfeng1116@email.arizona.edu>
2023-03-08 00:02:06 -0700

    user login signup pages

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return render_template('index.html')
+
+@app.route('/userlogin')
+def userlogin():
+    return render_template('userlogin.html')
