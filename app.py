e9994154575beac10e7527666622ef0374b28ac1
enfageorge <enfageorge@gmail.com>
2023-03-14 15:21:29 -0700

    EDIT : redirect home page to home page

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'PLACEHOLDER'  # TBA
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'PLACEHOLDER'  # TBA
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/userlogin')
 def userlogin():
