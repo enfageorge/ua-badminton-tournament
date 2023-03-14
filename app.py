88c0d1a88d1a5ed3182ae77f9461dc748dbac097
Rupal Jain <jainrupal@arizona.edu>
2023-03-14 02:27:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -14,3 +14,19 @@ def userlogin():
 @app.route('/usersignup')
 def usersignup():
     return render_template('usersignup.html')
+
+@app.route('/admindashboard')
+def admindashboard():
+    return render_template('admindashboard.html')
+
+@app.route('/admindashboard_form')
+def admindashboard_form():
+    return render_template('admindashboard_form.html')
+
+@app.route('/admindashboard_events')
+def admindashboard_events():
+    return render_template('admindashboard_events.html')
+
+@app.route('/admindashboard_matches')
+def admindashboard_matches():
+    return render_template('admindashboard_matches.html')
