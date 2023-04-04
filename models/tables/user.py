305ec9ba36023a6d1a194968be617a32cb65014e
Urvika-gola <urvika.gola@gmail.com>
2023-04-03 19:10:39 -0700

    changing user table name to users
    Signed-off-by: Urvika-gola <urvika.gola@gmail.com>

--- models/tables/user.py
+++ models/tables/user.py
@@ -9,7 +9,7 @@ Login credentials for Admin and Players are stored in 'Login' class
 
 
 class User(db.Model):
-    __tablename__ = "user"
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(20), nullable=False)
     last_name = db.Column(db.String(20), nullable=True)
