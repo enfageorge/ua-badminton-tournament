5fb1710b6d446140c42844b8eee4d8401e244d01
Rupal Jain <jainrupal@arizona.edu>
2023-04-04 02:15:43 -0700

    Update routes.py

--- modules/admin/routes.py
+++ modules/admin/routes.py
@@ -1,10 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
 from modules.admin.dashboard_home import get_player_details
 from modules.admin.dashboard_tournament import get_tournament_details
 from modules.admin.dashboard_events import get_event_details
 from modules.admin.dashboard_matches import get_matches_details
-
-
 
 admin_app = Blueprint("admin", __name__)
 
