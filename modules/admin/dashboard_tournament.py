3deafc536017560e18f91b562ee4fb67a81b5aae
Rupal Jain <jainrupal@arizona.edu>
2023-04-04 19:18:29 -0700

    Admin Dashboard Tournament page connected to backend

--- modules/admin/dashboard_tournament.py
+++ modules/admin/dashboard_tournament.py
@@ -1,33 +1,60 @@
-def get_tournament_details(username):
+def get_tournament_details(username, request):
+    if request.method == 'POST':
+        # INSERT to Tournament table
+        if 'name' in request.form:
+            name = request.form['name']
+            print(name)
+        if 'venue' in request.form:
+            venue = request.form['venue']
+            print(venue)
+        if 'location' in request.form:
+            location = request.form['location']
+            print(location)
+        if 'registration_open' in request.form:
+            registration_open = request.form['registration_open']
+            print(registration_open)
+        if 'registration_deadline' in request.form:
+            registration_deadline = request.form['registration_deadline']
+            print(registration_deadline)
+        if 'withdraw_deadline' in request.form:
+            withdraw_deadline = request.form['withdraw_deadline']
+            print(withdraw_deadline)
+        if 'tournament_start_date' in request.form:
+            tournament_start_date = request.form['tournament_start_date']
+            print(tournament_start_date)
+        if 'tournament_end_date' in request.form:
+            tournament_end_date = request.form['tournament_end_date']
+            print(tournament_end_date)
+        if 'events' in request.form:
+            events = request.form['events']
+            print(events)
+        if 'Regulations' in request.form:
+            Regulations = request.form['Regulations']
+            print(Regulations)
+        if 'announcements' in request.form:
+            announcements = request.form['announcements']
+            print(announcements)
+
+        # need to write data to DB.
+
     # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     # cursor.execute('SELECT * FROM accounts WHERE username = username)
     # account = cursor.fetchone()
     tournament_details = {
-        'id': 1,
-        'name': 'player',
-        'location': 'First name',
-        'registration_open': '04/01/2023',
-        'registration_closed': '04/15/2023',
-        'withdraw_deadline': '04/20/2023',
-        'tournament_start_date': '04/25/2023',
-        'tournament_end_date': '06/01/2023',
+        'name': '2023 Smashcat Tournament',
+        'venue': 'UoArizona Recreation Center, South Gym',
+        'location': 'Tucson, USA',
+        'contact': 'Julie Fan',
+        'contact_email': 'juliefan@arizona.edu',
+        'contact_phone': '520-123-3344',
+        'registration_open': '2023-08-01T10:00',
+        'registration_deadline': '2023-08-15T10:00',
+        'withdraw_deadline': '2023-08-20T10:00',
+        'tournament_start_date': '2023-08-25T10:00',
+        'tournament_end_date': '2023-09-01T10:00',
         'events': 'MS, MD, WS, WD',
-        'Regulations': 'The timeline of the events wll be posted soon.',
-        'announcements': 'No updated yet'
+        'Regulations': 'Fee per player: Fee includes rec center access, shuttles, lunch, and refreshments.',
+        'announcements': '-The registration deadline is on 04/15/2023. -MD and WD events are currently full.'
     }
 
-    # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    # cursor.execute('SELECT * FROM player WHERE userid = user_account['username'])
-    # player_account = cursor.fetchone()
-
-    # annoucements = {
-    #     'player_id': 123,
-    #     'seeding_score': 123,
-    #     'social_media_consent': True,
-    #     'competing_gender': 'f',
-    #     'phone_number': '5203360140',
-    #     'dob': '09-14-1998',  # MM-DD-YYYY
-    #     'club_name': 'club_name_placeholder'
-    # }
-
-    return tournament_details #| player_account
+    return tournament_details
