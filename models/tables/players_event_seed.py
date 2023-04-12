19a6266da04404e3e0081c2a4557991e5f2e05d9
Urvika-gola <urvika.gola@gmail.com>
2023-04-11 17:37:25 -0700

    Seeding table renamed, removed event-player
    Signed-off-by: Urvika-gola <urvika.gola@gmail.com>

--- models/tables/players_event_seed.py
+++ models/tables/players_event_seed.py
@@ -2,23 +2,26 @@ from app import db
 from sqlalchemy import ForeignKey
 
 """
-Class Seed, stores the seeding information for a player or team of players corresponding to an Event
+This table stores the (player that plays individually) or (players that form a team) for an EVENT.
+This table will also store the seeding value that the admin will update. 
+It's a many-to-many relationship between event & players.
+An event can be played by many players, A player can play many events.
 """
 
 
-class Seed(db.Model):
-    __tablename__ = "seed"
-    seed_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+class PlayersEventSeed(db.Model):
+    __tablename__ = "players_event_seed"
+    players_event_seed_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     player_1 = db.Column(db.Integer, ForeignKey('player.player_id'), nullable=False)
     player_2 = db.Column(db.Integer, ForeignKey('player.player_id'), nullable=True)
-    seeding_score = db.Column(db.Integer, nullable=False)
+    seeding_score = db.Column(db.Integer, nullable=True)
     event_id = db.Column(db.Integer, db.ForeignKey('event.event_id'))
     player_1_rel = db.relationship('Player', foreign_keys=[player_1], backref='seed_player_1')
     player_2_rel = db.relationship('Player', foreign_keys=[player_2], backref='seed_player_2')
 
     def __init__(self, player_1: int, seeding_score: int, event_id: int, player_2: int = None) -> None:
         """
-        Initialize a Seed object with the given attributes.
+        Initialize a PlayersEventSeed object with the given attributes.
         player_1: The ID of the first player.
         seeding_score: The seeding score for the team or individual.
         event_id: The ID of the event the seed is for.
@@ -30,6 +33,6 @@ class Seed(db.Model):
         self.event_id = event_id
 
     def __repr__(self) -> str:
-        """Return a string representation of the Seed object."""
-        return f"<Seed(seed_id={self.seed_id}, player_1={self.player_1}, player_2={self.player_2}, " \
-               f"seeding_score={self.seeding_score}, event_id={self.event_id})>"
+        """Return a string representation of the players_event_seed object."""
+        return f"<PlayersEventSeed(players_event_seed_id={self.seed_id}, player_1={self.player_1}, " \
+               f"player_2={self.player_2}, seeding_score={self.seeding_score}, event_id={self.event_id})>"
