4088afc6f48bd0e2e77ed0fa5dfb90b9e9eb430b
Urvika-gola <urvika.gola@gmail.com>
2023-04-13 11:43:20 -0700

    Updated database.py to remove eventplayer import
    Signed-off-by: Urvika-gola <urvika.gola@gmail.com>

--- database.py
+++ database.py
@@ -1,12 +1,11 @@
 from models.tables.event import Event
-from models.tables.event_player import EventPlayer
 from models.tables.login import Login
 from models.tables.match import Match
 from models.tables.permission import Permission
+from models.tables.user_permission import UserPermission
 from models.tables.player import Player
 from models.tables.result import Result
 from models.tables.tournament import Tournament
-from models.tables.user_permission import UserPermission
 from models.tables.users import Users
 
 
