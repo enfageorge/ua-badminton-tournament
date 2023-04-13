5a9d000588d458f8e23c1f48ff58cac6667028cb
Urvika-gola <urvika.gola@gmail.com>
2023-04-13 11:53:46 -0700

    Updated database.py for PlayersEventSeed
    Signed-off-by: Urvika-gola <urvika.gola@gmail.com>

--- database.py
+++ database.py
@@ -7,6 +7,7 @@ from models.tables.result import Result
 from models.tables.tournament import Tournament
 from models.tables.user_permission import UserPermission
 from models.tables.users import Users
+from models.tables.players_event_seed import PlayersEventSeed
 
 
 from flask_sqlalchemy import SQLAlchemy
