0e6555020cf4995ba5ea528374e9d07d3ce008a0
enfageorge <enfageorge@gmail.com>
2023-04-20 01:29:45 +0530

    update : fix event player import error

--- modules/admin/dashboard_home.py
+++ modules/admin/dashboard_home.py
@@ -1,18 +1,14 @@
-from models.models import db
+from app import db
 from models.tables.users import Users
-from models.tables.event_player import EventPlayer
 
 
 def get_player_roaster():
     db.session.expire_all()
     users = Users.query.all()
-    event_player_ids = EventPlayer.query.with_entities(EventPlayer.player_id).all()
     player_roaster = []
     s_no = 1
     for user in users:
         if user.player:
-            # and user.player_id in event_player_ids
-            # Uncomment this code once event player table has values
             player_roaster.append({
                 'id': s_no,
                 'username': user.login_id,
