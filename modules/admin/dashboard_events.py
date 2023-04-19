fd75314385a549697e41ac7f20bca8dfc4b4c622
junfeng1116 <junfeng1116@email.arizona.edu>
2023-04-19 12:49:00 -0700

    admin event change

--- modules/admin/dashboard_events.py
+++ modules/admin/dashboard_events.py
@@ -1,13 +1,12 @@
 from models.models import db
 from models.tables.users import Users
-from models.tables.event_player import EventPlayer
-# from models.tables.players_event_seed import PlayersEventSeed
-from models.tables.event import Event
+from models.tables.players_event_seed import PlayersEventSeed
+
 
 def get_event_details(username):
     db.session.expire_all()
     players_event_seed_ids = PlayersEventSeed.query.all()
-    eventids = Event.query.all()
+    # eventids = Event.query.all()
     ms_events_details = []
     ws_events_details = []
     md_events_details = []
@@ -62,6 +61,91 @@ def get_event_details(username):
             xd_no += 1
 
 
+    ms_events_details = [
+        {
+            'id': 1,
+            'player1': 'Joe',
+            'seed': '9'
+        },
+        {
+            'id': 2,
+            'player1': 'Michael',
+            'seed': '10'
+        }
+    ]
+    ws_events_details = [
+        {
+            'id': 1,
+            'player1': 'Zoey',
+            'seed': '12'
+        },
+        {
+            'id': 2,
+            'player1': 'Hanna',
+            'seed': '10'
+        }
+    ]
+    md_events_details = [
+        {
+            'id': 1,
+            'player1': 'Neil',
+            'player2': 'Jon',
+            'seed': '15'
+        },
+        {
+            'id': 2,
+            'player1': 'Joey',
+            'player2': 'Dan',
+            'seed': '14'
+        }
+    ]
+    wd_events_details = [
+        {
+            'id': 1,
+            'player1': 'Jill',
+            'player2': 'Rebecca',
+            'seed': '18'
+        },
+        {
+            'id': 2,
+            'player1': 'Rina',
+            'player2': 'Jennifer',
+            'seed': '15'
+        }
+    ]
+    xd_events_details = [
+        {
+            'id': 1,
+            'player1': 'Kaden',
+            'player2': 'Luna',
+            'seed': '20'
+        },
+        {
+            'id': 1,
+            'player1': 'Lilly',
+            'player2': 'Bruce',
+            'seed': '22'
+        }
+    ]
+    u19_events_details = [
+        {
+            'id': 1,
+            'player1': 'Grace',
+            'seed': '9'
+        },
+        {
+            'id': 2,
+            'player1': 'Malaya',
+            'seed': '10'
+        }
+    ]
+    u17_events_details = [
+        {
+            'id': 1,
+            'player1': 'Mike',
+            'seed': '10'
+        }
+    ]
     return {"ms_events_details": ms_events_details,
             "ws_events_details": ws_events_details,
             "md_events_details": md_events_details,
