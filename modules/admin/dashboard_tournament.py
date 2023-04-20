17b6ffb4b63b0fecc1e8443b54d8aeac5a9238b1
Rupal Jain <jainrupal@arizona.edu>
2023-04-20 14:31:36 -0700

    Update dashboard_tournament.py

--- modules/admin/dashboard_tournament.py
+++ modules/admin/dashboard_tournament.py
@@ -1,60 +1,144 @@
-def get_tournament_details(username, request):
-    if request.method == 'POST':
+from flask import session
+from models.models import db
+from models.tables.tournament import Tournament
+from models.tables.event import Event
+from models.tables.match import Match
+from models.tables.login import Login
+from models.tables.permission import Permission
+from models.tables.player import Player
+from models.tables.result import Result
+from models.tables.user_permission import UserPermission
+from models.tables.users import Users
+from models.tables.players_event_seed import PlayersEventSeed
+import json
+
+
+def post_tournament_details(request):
+    db.session.expire_all()
+    tournament_name = request.form.get('tournament_name')
+    tournament = Tournament.query.filter_by(tournament_name=tournament_name).first()
+    events = Event.query.all()
+    event_DB = [event.event_name for event in events]
+    if tournament:
+        if tournament.tournament_name != request.form['tournament_name']:
+            tournament.tournament_name = request.form['tournament_name']
+        if tournament.location != request.form['location']:
+            tournament.location = request.form['location']
+        if tournament.registration_open.strftime('%Y-%m-%dT%H:%M') != request.form['registration_open']:
+            tournament.registration_open = request.form['registration_open']
+        if tournament.registration_closed.strftime('%Y-%m-%dT%H:%M') != request.form['registration_closed']:
+            registration_closed = request.form['registration_closed']
+        if tournament.tournament_start_date.strftime('%Y-%m-%dT%H:%M') in request.form['tournament_start_date']:
+            tournament.tournament_start_date = request.form['tournament_start_date']
+        if tournament.tournament_end_date.strftime('%Y-%m-%dT%H:%M') in request.form['tournament_end_date']:
+            tournament.tournament_end_date = request.form['tournament_end_date']
+        if tournament.announcement != request.form['announcements']:
+            tournament.announcement = request.form['announcements']
+
+        events_list_request = json.loads(request.form.getlist('events')[0])
+        events_request = [key for event_dict in events_list_request for key in event_dict.keys()]
+
+        events_list = ['MS', 'MD', 'WS', 'WD', 'XD', 'U19', 'U17']
+        for event in events_list:
+            if event in event_DB and event in events_request:
+                event_dict = next((d for d in events_list_request if d.get(event)), None)
+                event_entry = Event.query.filter_by(event_name=event).first()
+                if event_dict:
+                    if event_entry.gender_allowed != event_dict[event]['gender_allowed']:
+                        event_entry.gender_allowed = event_dict[event]['gender_allowed']
+                    if event_entry.max_participants_allowed != event_dict[event]['max_participants_allowed']:
+                        event_entry.max_participants_allowed = event_dict[event]['max_participants_allowed']
+                    db.session.commit()
+            elif event in event_DB and event not in events_request:
+                event_entry = Event.query.filter_by(event_name=event).first()
+                db.session.delete(event_entry)
+                db.session.commit()
+            elif event not in event_DB and event in events_request:
+                event_dict = next((d for d in events_list_request if d.get(event)), None)
+                if event_dict:
+                    new_event = Event(event, event_dict[event]['gender_allowed'], event_dict[event]['max_participants_allowed'])
+                    db.session.add(new_event)
+                    db.session.commit()
+    else:
+        tournament_name, location, registration_open, registration_closed, tournament_start_date, tournament_end_date, announcements = '', '', '', '', '', '', ''
         # INSERT to Tournament table
-        if 'name' in request.form:
-            name = request.form['name']
-            print(name)
-        if 'venue' in request.form:
-            venue = request.form['venue']
-            print(venue)
+        if 'tournament_name' in request.form:
+            tournament_name = request.form['tournament_name']
         if 'location' in request.form:
             location = request.form['location']
-            print(location)
         if 'registration_open' in request.form:
             registration_open = request.form['registration_open']
-            print(registration_open)
-        if 'registration_deadline' in request.form:
-            registration_deadline = request.form['registration_deadline']
-            print(registration_deadline)
-        if 'withdraw_deadline' in request.form:
-            withdraw_deadline = request.form['withdraw_deadline']
-            print(withdraw_deadline)
+        if 'registration_closed' in request.form:
+            registration_closed = request.form['registration_closed']
         if 'tournament_start_date' in request.form:
             tournament_start_date = request.form['tournament_start_date']
-            print(tournament_start_date)
         if 'tournament_end_date' in request.form:
             tournament_end_date = request.form['tournament_end_date']
-            print(tournament_end_date)
-        if 'events' in request.form:
-            events = request.form['events']
-            print(events)
-        if 'Regulations' in request.form:
-            Regulations = request.form['Regulations']
-            print(Regulations)
         if 'announcements' in request.form:
             announcements = request.form['announcements']
-            print(announcements)
+        if 'events' in request.form:
+        # Create a new Tournament object with the details
+        new_tournament = Tournament(tournament_name, location, registration_open, registration_closed,
+                                    tournament_start_date, tournament_end_date, announcements)
+        db.session.add(new_tournament)
 
-        # need to write data to DB.
+        events_list = json.loads(request.form.getlist('events')[0])
+        for event_dict in events_list:
+            for key in event_dict.keys():
+                new_event = Event(key, event_dict[key]['gender_allowed'], event_dict[key]['max_participants_allowed'])
+                db.session.add(new_event)
 
-    # cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-    # cursor.execute('SELECT * FROM accounts WHERE username = username)
-    # account = cursor.fetchone()
+    db.session.commit()
+
+    db.session.expire_all()
+    events = Event.query.all()
+    event_name = [event.event_name for event in events]
+    names_string = ", ".join(event_name)
+    # Return the tournament details
     tournament_details = {
-        'name': '2023 Smashcat Tournament',
-        'venue': 'UoArizona Recreation Center, South Gym',
-        'location': 'Tucson, USA',
-        'contact': 'Julie Fan',
-        'contact_email': 'juliefan@arizona.edu',
-        'contact_phone': '520-123-3344',
-        'registration_open': '2023-08-01T10:00',
-        'registration_deadline': '2023-08-15T10:00',
-        'withdraw_deadline': '2023-08-20T10:00',
-        'tournament_start_date': '2023-08-25T10:00',
-        'tournament_end_date': '2023-09-01T10:00',
-        'events': 'MS, MD, WS, WD',
-        'Regulations': 'Fee per player: Fee includes rec center access, shuttles, lunch, and refreshments.',
-        'announcements': '-The registration deadline is on 04/15/2023. -MD and WD events are currently full.'
+        'tournament_name': tournament.tournament_name,
+        'location': tournament.location,
+        'registration_open': tournament.registration_open,
+        'registration_closed': tournament.registration_closed,
+        'tournament_start_date': tournament.tournament_start_date,
+        'tournament_end_date': tournament.tournament_end_date,
+        'event': names_string,
+        'announcements': tournament.announcement
     }
+    return tournament_details
 
+
+def get_tournament_details(request):
+    tournament_details = {
+        'tournament_name': '',
+        'location': '',
+        'registration_open': '',
+        'registration_deadline': '',
+        'tournament_start_date': '',
+        'tournament_end_date': '',
+        'events': [],
+        'Regulations': '',
+        'announcements': ''
+    }
+    db.session.expire_all()
+    tournament = Tournament.query.all()
+    events = Event.query.all()
+    if bool(tournament):
+        events_list = []
+        for event in events:
+            events_list.append({
+                'name': event.event_name,
+                'gender_allowed': event.gender_allowed,
+                'max_participants_allowed': event.max_participants_allowed
+            })
+        tournament_details = {
+            'tournament_name': tournament[0].tournament_name,
+            'location': tournament[0].location,
+            'registration_open': tournament[0].registration_open.strftime('%Y-%m-%dT%H:%M'),
+            'registration_closed': tournament[0].registration_closed.strftime('%Y-%m-%dT%H:%M'),
+            'tournament_start_date': tournament[0].tournament_start_date.strftime('%Y-%m-%dT%H:%M'),
+            'tournament_end_date': tournament[0].tournament_end_date.strftime('%Y-%m-%dT%H:%M'),
+            'events': events_list,
+            'announcements': tournament[0].announcement
+        }
     return tournament_details
