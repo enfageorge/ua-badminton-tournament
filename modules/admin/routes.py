4268fdac71178bdd270b3e233648b32ff2fbf6ad
enfageorge <enfageorge@gmail.com>
2023-04-24 11:36:19 -0700

    add : function call on make draw click

--- modules/admin/routes.py
+++ modules/admin/routes.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect
 
 from modules.admin.dashboard_home import get_player_roaster
 from modules.admin.dashboard_tournament import get_tournament_details
 from modules.admin.dashboard_events import get_event_details, assign_seeding
 from modules.admin.dashboard_matches import get_matches_details
+from modules.admin.draws import make_draws
 
 from modules.decoraters import admin_login_required
 
@@ -14,7 +15,7 @@ admin_app = Blueprint("admin", __name__)
 @admin_login_required
 def admin_dashboard():
     player_details = get_player_roaster()
-    return render_template('admin/admin_dashboard.html', msg = player_details)
+    return render_template('admin/admin_dashboard.html', msg=player_details)
 
 
 @admin_app.route('/admin/tournament', methods=['GET', 'POST'])
@@ -38,6 +39,13 @@ def enter_seed():
         event_details = get_event_details()
         seed_updated_details = assign_seeding(request, event_details)
         return render_template('admin/admin_events.html', msg=seed_updated_details)
+
+
+@admin_app.route('/admin/make_draw', methods=['GET','POST'])
+@admin_login_required
+def make_draw():
+    make_draws()
+    return redirect('/draws')
 
 
 @admin_app.route('/admin/matches')
