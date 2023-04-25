8471b82ac9e42a0c88649674a2ee6e9f63e711d1
Rupal Jain <jainrupal@arizona.edu>
2023-04-25 01:40:06 -0700

    Update dashboard_home.py

--- modules/player/dashboard_home.py
+++ modules/player/dashboard_home.py
@@ -29,7 +29,7 @@ def get_player_details(user_id):
 
     events = Event.query.all()
     event_DB = [event.event_name for event in events]
-    event_list.append((event_DB))
+    # event_list.append((event_DB))
 
     for event_regis in events_registered:
         event = Event.query.filter_by(event_id=event_regis.event_id).first()
@@ -40,18 +40,21 @@ def get_player_details(user_id):
             player2 = player2.login_id
         event_list.append({event_name: player2})
 
+    print("List player registeres for: ",event_list)
     player_details = {
         'competing_gender': player.competing_gender,
         'phone_number': player.phone_number,
         'dob': player.dob,
         'club_name': player.club_name,
-        'event': event_list
+        'event': event_list,
+        'all_events': event_DB
     }
 
     return {**user_details, **player_details}
 
 
 def edit_player_details(request, user_id):
+    print("POST: ", request.form)
     msg = ''
     current_user = Users.query.get(user_id)
 
@@ -74,12 +77,17 @@ def edit_player_details(request, user_id):
     if request.form['dob']: player_obj.dob = request.form['dob']
     if request.form['club_name']: player_obj.club_name = request.form['club_name']
 
-    events = PlayersEventSeed.query.all()
+    # events = PlayersEventSeed.query.all()
+    event_registered = request.form.get('events')
+    event_registered = json.loads(event_registered)
+    print(event_registered, list(event_registered.keys()))
+    # for e in event_registered:
+    #     print(e)
 
     events = Event.query.all()
     event_list = []
     for event in events:
-        gender_allowed_list = [g.lower() for g in event.gender_allowed.split(',')]
+        gender_allowed_list = [g for g in event.gender_allowed.split(',')]
         if player_obj.competing_gender in gender_allowed_list:
             event_list.append(event.event_name)
 
@@ -87,27 +95,43 @@ def edit_player_details(request, user_id):
     event_ids = [[event.event_id, event.event_name] for event in
                  Event.query.filter(Event.event_name.in_(event_list)).all()]
 
+    print(Users.query.all())
     # Loop over the events and update/create the entries in PlayersEventSeed
     for event_id in event_ids:
+        print(event_id, event_id[1])
         # Check if an entry already exists
         event_seed = PlayersEventSeed.query.filter_by(player_1=player_obj.player_id, event_id=event_id[0]).first()
 
+        print(event_seed)
         # If an entry exists, update the fields
         if event_seed:
-            player_2_id = request.form.get(event_id[1] + '_partner', None)
-            if request.form[event_id[1]] not in ['WS', 'MS', 'U19', 'U17'] and player_2_id:
-                player_2 = Users.query.filter_by(login_id=player_2_id).first()
-                event_seed.player_2 = player_2.id
+            print("present:")
+            print(event_id[1] in event_registered)
+            if(event_id[1] in event_registered):
+                player_2_id = event_registered[event_id[1]]
+                print(player_2_id['partner'])
+                if event_id[1] not in ['WS', 'MS', 'U19', 'U17'] and player_2_id != 'Singles':
+                    player_2 = Users.query.filter_by(login_id=player_2_id['partner']).first()
+                    print(player_2)
+                    event_seed.player_2 = player_2.id
+                else:
+                    event_seed.player_2 = None
             else:
-                event_seed.player_2 = None
+                db.session.delete(event_seed)
         # If an entry does not exist, create a new instance of PlayersEventSeed and add it to the database
         else:
-            if request.form[event_id[1]] not in ['WS', 'MS', 'U19', 'U17']:
+            print("New:")
+            if request.form.get(event_id[1]) not in ['WS', 'MS', 'U19', 'U17']:
                 player_2_id = request.form.get(event_id[1] + '_partner', None)
+                print(player_2_id)
                 if player_2_id:
                     player_2 = Users.query.filter_by(login_id=player_2_id).first()
+                    print(player_2)
+                    player_2 = player_2.id
+                else:
+                    player_2 = None
                 new_event_seed = PlayersEventSeed(player_1=player_obj.player_id,
-                                                  player_2=player_2_id.id,
+                                                  player_2=player_2,
                                                   seeding_score=0, event_id=event_id[0])
                 db.session.add(new_event_seed)
             else:
