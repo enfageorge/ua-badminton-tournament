2c705a434cf6392eda810423e4cd8e1cf00493f1
Rupal Jain <jainrupal@arizona.edu>
2023-04-25 03:40:31 -0700

    Update routes.py

--- modules/web/routes.py
+++ modules/web/routes.py
@@ -1,21 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from modules.admin.dashboard_home import get_player_roaster
 from modules.admin.dashboard_events import get_event_details
 from modules.admin.dashboard_matches import get_public_matches_details
 from modules.web.draw import return_events_with_draws
+from modules.admin.dashboard_tournament import get_tournament_details
 
 web_app = Blueprint("web", __name__)
 
 
 @web_app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('web/home.html')
+    tournament_overview = get_tournament_details(request)
+    return render_template('web/home.html', msg = tournament_overview)
 
 
 @web_app.route('/tournament')
 def tournament():
-    return render_template('web/tournament.html')
+    tournament_details = get_tournament_details(request)
+    return render_template('web/tournament.html', msg = tournament_details)
 
 
 @web_app.route('/players')
